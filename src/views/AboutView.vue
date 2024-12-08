8231b542173ed8ff0d988def05497e9387ee10a9
ChrisWMichel <christopherw.michel@gmail.com>
2024-12-08 09:10:50 -0600

    update about me

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,48 @@
 <template>
-  <div>
-    <h1>About</h1>
+  <div class="text-gray-900 bg-gray-100">
+    <main class="container px-6 py-8 mx-auto">
+      <div class="p-6 bg-white rounded-lg shadow-lg">
+        <p class="mb-4 text-2xl">Let me start off with a funny story.</p>
+        <p class="mb-4 text-lg">
+          When I was young, ambitious, and full of energy, I decided to take
+          every programming class McHenry County College had to offer. I told
+          myself I was going to conquer them all! By the time I graduated with
+          my A.A.S., I had accumulated 118 credits. Ironically, despite all the
+          knowledge, I wasn’t proficient in any particular language. This
+          experience taught me the importance of focused learning and the skill
+          of learning how to learn.
+        </p>
+        <p class="mb-4 text-lg">
+          In 2007, I decided to move away from Visual Basic as desktop
+          applications started to fade with the rise of web applications.
+          Determined to adapt, I chose to focus on PHP, JavaScript, and
+          HTML/CSS. Over time, I also zeroed in on frameworks like Laravel,
+          VueJS, and Tailwind to broaden my expertise.
+        </p>
+        <p class="mb-4 text-lg">
+          Though I needed to accept diverse roles to sustain my career, my
+          proficiency in PHP and JavaScript has been invaluable at Orbital
+          Engineering and Sporty’s. However, I am still on the lookout for
+          opportunities where I can fully leverage my skills in Laravel and
+          VueJS.
+        </p>
+        <p class="text-xl font-bold">
+          So, if you’re reading this and have a position that fits my criteria,
+          let’s create some magic together!
+        </p>
+        <p class="my-4 text-xl">
+          As a side note, to point out how much time I spend learning, Udemy
+          sent me an email letting me know how many minutes I spent in learning
+          in 2024.
+          <a
+            target="_blank"
+            class="font-bold text-orange-800"
+            href="https://mi.udemy.com/p/rp/40eebe8e432ee0a5.png?mi_u=61e958adf614f60d2e6d2037&Learner_Type=Learning%20Legend&Learner_Type_prefix=I%27m%20a&minutes=Total%20minutes%20learned:&XXX=7980&Content-5_7-LT_img_float-RT_txt_subheadline-copy_2_A=That%27s%20more%20than&Content-5_7-LT_img_float-RT_txt_subheadline-copy_2_B=of%20other%20learners.&percent_XX_value=99%"
+            >Check it out!</a
+          >
+        </p>
+      </div>
+    </main>
   </div>
 </template>
 
