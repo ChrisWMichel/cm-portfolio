b148f8cd932df3dd86a20f157f86ff040b711f2d
ChrisWMichel <christopherw.michel@gmail.com>
2024-12-22 14:50:58 -0600

    Adding fullstack project

--- src/components/FullStack.vue
+++ src/components/FullStack.vue
@@ -13,6 +13,67 @@
       I'm currently working on a new fullstack application that I plan to deploy
       to Github. I will update this page with the link once it's ready.
     </p>
+
+    <div class="flex items-center justify-center">
+      <div
+        class="grid grid-cols-1 grid-rows-6 gap-4 p-4 lg:grid-cols-2 lg:grid-rows-3"
+      >
+        <div class="p-4 bg-gray-200 dark:bg-gray-700">
+          <div
+            class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+          >
+            <a href="https://www.cwm-portfolio.com/" target="_blank">
+              <img
+                class="rounded-t-lg"
+                src="../assets/images/students_management.png"
+                alt=""
+              />
+            </a>
+            <div class="p-5">
+              <a href="https://www.cwm-portfolio.com/" target="_blank">
+                <h5
+                  class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                >
+                  Students Management
+                </h5>
+              </a>
+              <div class="text-lg text-gray-400">
+                Created:
+                <span class="font-semibold">Dec. 2024</span>
+              </div>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                This app shows off CRUD operations on students and roles data.
+                It is built with Laravel, Vue 3, and Tailwind CSS. Sqlite is
+                used for the database. Authorization and permissions (assigning
+                roles to users) are implemented by scratch.
+              </p>
+              <a
+                href="https://github.com/ChrisWMichel/students-management"
+                target="_blank"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                GitHub
+                <svg
+                  class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
