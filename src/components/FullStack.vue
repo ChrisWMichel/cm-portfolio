08aa8f0d5956783ddcf140652d3034cae07e93be
ChrisWMichel <christopherw.michel@gmail.com>
2025-04-06 20:01:11 -0500

    added another fullstack, boss dog

--- src/components/FullStack.vue
+++ src/components/FullStack.vue
@@ -3,21 +3,20 @@
     <h1 class="text-3xl">FullStack</h1>
     <p class="text-2xl font-bold">
       I started learning Laravel in 2015. By 2017, I had built several fullstack
-      applications and published them after deploying them to Github. The bigger
-      an application is, the more time it takes to maintain them. By 2018, I had
-      gotten a job at Orbital Engineering and no long had the time to maintain
-      these applications. I have since taken them down as they are antiquated.
-      However, these apps are still on Github.
+      applications and published them after deploying them to Github. The bigger an
+      application is the more time it takes to maintain them. By 2018, I had gotten a job
+      at Orbital Engineering and no longer had the time to maintain these applications. I
+      have since taken them down as they are antiquated. However, these apps are still on
+      Github.
     </p>
     <p class="text-2xl font-bold">
-      I'm currently working on a new fullstack application that I plan to deploy
-      to Github. I will update this page with the link once it's ready.
+      I'm currently working on new fullstack applications to get back into the swing of
+      things. I plan to deploy each site to Github as well as here. I will update this
+      page with the links once they're ready.
     </p>
 
     <div class="flex items-center justify-center">
-      <div
-        class="grid grid-cols-1 grid-rows-6 gap-4 p-4 lg:grid-cols-2 lg:grid-rows-3"
-      >
+      <div class="grid grid-cols-1 grid-rows-6 gap-4 p-4 lg:grid-cols-2 lg:grid-rows-3">
         <div class="p-4 bg-gray-200 dark:bg-gray-700">
           <div
             class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
@@ -42,10 +41,10 @@
                 <span class="font-semibold">Dec. 2024</span>
               </div>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                This app shows off CRUD operations on students and roles data.
-                It is built with Laravel, Vue 3, and Tailwind CSS. Sqlite is
-                used for the database. Authorization and permissions (assigning
-                roles to users) are implemented by scratch.
+                This app shows off CRUD operations on students and roles data. It is built
+                with Laravel, Vue 3, and Tailwind CSS. Sqlite is used for the database.
+                Authorization and permissions (assigning roles to users) are implemented
+                by scratch.
               </p>
               <a
                 href="https://github.com/ChrisWMichel/students-management"
@@ -76,21 +75,11 @@
           <div
             class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
           >
-            <a
-              href="https://realtor.michel-5.com/"
-              target="_blank"
-            >
-              <img
-                class="rounded-t-lg"
-                src="../assets/images/Realtor.png"
-                alt=""
-              />
+            <a href="https://realtor.michel-5.com/" target="_blank">
+              <img class="rounded-t-lg" src="../assets/images/Realtor.png" alt="" />
             </a>
             <div class="p-5">
-              <a
-                href="https://realtor.michel-5.com/"
-                target="_blank"
-              >
+              <a href="https://realtor.michel-5.com/" target="_blank">
                 <h5
                   class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
                 >
@@ -102,11 +91,10 @@
                 <span class="font-semibold">Jan. 2025</span>
               </div>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                This app allows user to post and make offers on homes. A user
-                can make an offer on a home. Accept the offer or decline. The
-                app also has a feature where one can filter through the homes.
-                It is built with Laravel, Vue 3, and Tailwind CSS. MySQL is used
-                for the database.
+                This app allows user to post and make offers on homes. A user can make an
+                offer on a home. Accept the offer or decline. The app also has a feature
+                where one can filter through the homes. It is built with Laravel, Vue 3,
+                and Tailwind CSS. MySQL is used for the database.
               </p>
               <a
                 href="https://github.com/ChrisWMichel/Laravel-realestate"
@@ -137,21 +125,11 @@
           <div
             class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
           >
-            <a
-              href="https://michel-5.com/"
-              target="_blank"
-            >
-              <img
-                class="rounded-t-lg"
-                src="../assets/images/t-shirts.png"
-                alt=""
-              />
+            <a href="https://michel-5.com/" target="_blank">
+              <img class="rounded-t-lg" src="../assets/images/t-shirts.png" alt="" />
             </a>
             <div class="p-5">
-              <a
-                href="https://michel-5.com/"
-                target="_blank"
-              >
+              <a href="https://michel-5.com/" target="_blank">
                 <h5
                   class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
                 >
@@ -163,14 +141,68 @@
                 <span class="font-semibold">Feb. 2025</span>
               </div>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                This is a Laravel/Vue.js ecommerce app that has a lot of
-                features most e-commerce apps have. Login, logout, profile, and
-                order history. Admin side to manage products. Add Coupon
-                function (admin side). Add, approve, and display reviews. User
-                can also add, edit, delete their own reviews.
+                This is a Laravel/Vue.js ecommerce app that has a lot of features most
+                e-commerce apps have. Login, logout, profile, and order history. Admin
+                side to manage products. Add Coupon function (admin side). Add, approve,
+                and display reviews. User can also add, edit, delete their own reviews.
               </p>
               <a
                 href="https://github.com/ChrisWMichel/vue-laravel-ecom"
+                target="_blank"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                GitHub
+                <svg
+                  class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 bg-gray-200 dark:bg-gray-700">
+          <div
+            class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+          >
+            <a href="https://boss-dog.michel-5.com/" target="_blank">
+              <img
+                class="rounded-t-lg"
+                src="../assets/images/boss_dog_screenshot.png
+"
+                alt=""
+              />
+            </a>
+            <div class="p-5">
+              <a href="https://boss-dog.michel-5.com/" target="_blank">
+                <h5
+                  class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                >
+                  Boss Dog E-commerce
+                </h5>
+              </a>
+              <div class="text-lg text-gray-400">
+                Created:
+                <span class="font-semibold">April 2025</span>
+              </div>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                This is a Laravel Ecommerce project. With all the features you need to
+                build a modern ecommerce website. It is built with Laravel, Vue.js (with
+                Pinia), and Tailwind CSS. It is a complete ecommerce solution that you can
+                use as a starting point for your own project.
+              </p>
+              <a
+                href="https://github.com/ChrisWMichel/Boss_Dog"
                 target="_blank"
                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
